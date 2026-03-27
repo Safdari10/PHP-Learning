a75cf198c19b1bef0184912e0ea32e4928ea7821
Safdari10 <zakersafdari@gmail.com>
2026-03-27 19:14:04 +1300

    Add questionMarks function to check for pairs of numbers summing to 10 with specific question mark conditions

--- stringPattern.php
+++ stringPattern.php
@@ -47,4 +47,58 @@ function wordPattern($pattern, $str): bool
         }
         return true;
     }
+
+    const questionMarks = (str) => {
+        let lastNum = null;
+        let questionCount = 0;
+        let foundPair = false;
+
+        for (let char of str) {
+            if (char === ?) {
+                questionCount++;
+            } else if (!isNaN(char)) {
+                let num = Number(char)
+
+                if (lastNum !== null) {
+                    if(lastNum + num === 10) {
+                        foundPair = true;
+                        if (questionCount !== 3) {
+                            return false;
+                        }
+                    }
+                }
+                lastNum = num;
+                questionCount = 0;
+            }
+        }
+        return foundPair ? "true" : "false"
+    }
 </script>
+
+<?php
+function questionMarks($str): string
+{
+    $prevNum = null;
+    $count = 0;
+    $pair = false;
+    $chars = preg_split('/\s+/', $str);
+
+    foreach ($chars as $char) {
+        if ($char === "?" ) {
+            $count++;
+        } else if (is_numeric($char)) {
+            $num = (int)$char;
+            if ($prevNum !== null) {
+                if ($prevNum + $num === 10) {
+                    $pair = true;
+                    if ($count !== 3) {
+                        return "false";
+                    }
+                }
+            }
+            $prevNum = $num;
+            $count = 0;
+        }
+    }
+    return $pair ? "true" : "false";
+}
