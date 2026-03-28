11471ce219de189bb32b02a854af5d28e4ee54f4
Safdari10 <zakersafdari@gmail.com>
2026-03-28 16:46:34 +1300

    Add isAnagram2 function for improved anagram checking using a map

--- codingChallenge.js
+++ codingChallenge.js
@@ -75,5 +75,20 @@ const isAnagram1 = (str1, str2) => {
     return sortedStr1 === sortedStr2
 }
 
+const isAnagram2 = (str1, str2) => {
+    if (str1.length !== str2.length) return false;
+    let map1 = new Map();
 
+    for (let char of str1.toLowerCase()) {
+        map1.set(char, (map1.get(char) ?? 0) + 1);
+    }
+
+    for (let char of str2.toLowerCase()) {
+        if (!map1.has(char) || map1.get(char) === 0) {
+            return false;
+        }
+        map1.set(char, map1.get(char) - 1)
+    }
+    return true;
+}
 
