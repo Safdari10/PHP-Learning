1f4b24048778170fb2aa13959b0458a180af6329
Safdari10 <zakersafdari@gmail.com>
2026-03-28 13:14:58 +1300

    Add firstNonRepeat function to find the first non-repeating character in a string

--- codingChallenge.js
+++ codingChallenge.js
@@ -12,3 +12,19 @@ const countVowels = (str) => {
     }
     return count;
 }
+
+// Problem 2
+const firstNonRepeat = (str) => {
+    let map = new Map();
+
+    for (let char of str) {
+        map.set(char, (map.get(char) ?? 0) + 1)
+    }
+    for (let char of str) {
+        if (map.get(char) === 1) {
+            return char;
+        }
+    }
+    return null;
+}
+console.log(firstNonRepeat('aabbcdefff'));
