d31350251fb2fe0090678dcf5e8d4bae5de83197
Safdari10 <zakersafdari@gmail.com>
2026-03-28 13:25:36 +1300

    Add twoSum function to find indices of two numbers that sum to a target

--- codingChallenge.js
+++ codingChallenge.js
@@ -28,3 +28,15 @@ const firstNonRepeat = (str) => {
     return null;
 }
 
+// Problem 3
+const twoSum = (arr, target) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+    return null;
+}
+
