bc369936c9f720d54cfa32d089bd628d33a801f0
Safdari10 <zakersafdari@gmail.com>
2026-03-28 16:46:14 +1300

    Add isAnagram1 function to check if two strings are anagrams

--- codingChallenge.js
+++ codingChallenge.js
@@ -66,3 +66,14 @@ const containsDuplicate = (arr) => {
     }
     return false;
 }
+
+// Problem 5
+const isAnagram1 = (str1, str2) => {
+    if (str1.length !== str2.length) return false
+    let sortedStr1 = str1.split('').sort().join('');
+    let sortedStr2 = str2.split('').sort().join('');
+    return sortedStr1 === sortedStr2
+}
+
+
+
