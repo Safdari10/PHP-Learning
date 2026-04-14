017918d5ac8ff499de54de1c05c01d510ca6a37a
Safdari10 <zakersafdari@gmail.com>
2026-04-14 17:04:31 +1200

    Add findLargest function to return the largest number in an array

--- learnCodeTest.js
+++ learnCodeTest.js
@@ -9,3 +9,15 @@ const isEven = (num) => num % 2 === 0;
 const reverseString = (str) => {
     return str.split('').reverse().join('');
 }
+
+// Question 4:
+const findLargest = (arr) => {
+    let largest = arr[0];
+
+    for (let num of arr) {
+        if (num > largest) {
+            largest = num;
+        }
+    }
+    return largest;
+}
