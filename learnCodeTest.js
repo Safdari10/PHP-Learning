a4838915c32dd8e12d02d63da12e42133c41c407
Safdari10 <zakersafdari@gmail.com>
2026-04-14 17:02:05 +1200

    Add reverseString function to reverse a given string

--- learnCodeTest.js
+++ learnCodeTest.js
@@ -4,3 +4,8 @@ const sumTwoNumbers = (a, b) => a + b;
 
 // Question 2:
 const isEven = (num) => num % 2 === 0;
+
+// Question 3:
+const reverseString = (str) => {
+    return str.split('').reverse().join('');
+}
