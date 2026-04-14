fac10e4569b0d785c2e2e16fe6f8ff04f3c5b646
Safdari10 <zakersafdari@gmail.com>
2026-04-14 17:11:15 +1200

    Add countVowels function to count the number of vowels in a string

--- learnCodeTest.js
+++ learnCodeTest.js
@@ -21,3 +21,16 @@ const findLargest = (arr) => {
     }
     return largest;
 }
+
+// Question 5:
+const countVowels = (str) => {
+    const vowels = ["a","e","i","o","u"];
+    let count = 0;
+
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
