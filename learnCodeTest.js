ed76c3fb8c88942c99cb68248da63a57863face3
Safdari10 <zakersafdari@gmail.com>
2026-04-14 17:35:46 +1200

    Add hasPairWithSum function to check if any two numbers in an array sum to a target value

--- learnCodeTest.js
+++ learnCodeTest.js
@@ -71,3 +71,15 @@ const removeDuplicates = (arr) => {
     }
     return result;
 }
+
+// Question 9:
+const hasPairWithSum = (arr, target) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
