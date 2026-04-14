93e9f580caa9135608cbdfbd017b833dff9deac5
Safdari10 <zakersafdari@gmail.com>
2026-04-14 17:13:36 +1200

    Add countOccurrences function to count the occurrences of a target number in an array

--- learnCodeTest.js
+++ learnCodeTest.js
@@ -34,3 +34,15 @@ const countVowels = (str) => {
     }
     return count;
 }
+
+// Question 6:
+const countOccurrences = (arr, target) => {
+    let count = 0;
+
+    for (let num of arr) {
+        if (num === target) {
+            count++;
+        }
+    }
+    return count;
+}
