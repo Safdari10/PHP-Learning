88ae4dfecb46e2cc0acfbcc8556aa50d903383f4
Safdari10 <zakersafdari@gmail.com>
2026-04-19 14:55:33 +1200

    Add birthday function to count ways to segment an array into parts summing to a given value

--- hackRank/hackRank.js
+++ hackRank/hackRank.js
@@ -206,3 +206,20 @@ function twoArrays(k, A, B) {
 
   return "YES";
 }
+
+
+// Question 10:
+function birthday(s, d, m) {
+  let count = 0;
+
+  for (let i = 0; i <= s.length - m; i++) {
+    let sum = 0;
+    for (let j = 0; j < m; j++) {
+      sum += s[i + j];
+    }
+    if (sum === d) {
+      count++;
+    }
+  }
+  return count;
+}
