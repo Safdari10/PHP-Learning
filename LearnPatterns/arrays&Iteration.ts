22887ee297675dd76cb1d57b2ba11cad6dcea3ee
Safdari10 <zakersafdari@gmail.com>
2026-05-03 12:13:01 +1200

    Add findMin function to return the minimum value from an array

--- LearnPatterns/arrays&Iteration.ts
+++ LearnPatterns/arrays&Iteration.ts
@@ -1,14 +1,27 @@
-
 // Problem 1:
 function findMax(arr: number[]): number {
-    if (arr.length === 0) return NaN;
-    let max = arr[0];
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
+  if (arr.length === 0) return NaN;
+  let max = arr[0];
 
-    return max;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
+
+// Problem 2:
+function findMin(arr: number[]): number {
+  if (arr.length === 0) return NaN;
+  let min = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  return min;
 }
